dec7b5df7f24ea3ec0649b7203f46a39a7901973
Julien Charbonneau <julien_charbonneau@live.ca>
2016-05-14 22:25:48 -0400

    MainMenu done

--- Night Shift/Assets/Scripts/MainMenu/MenuManager.cs
+++ Night Shift/Assets/Scripts/MainMenu/MenuManager.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+    public GameObject storyMenu;
+
+
+    public void SelectMainMenu() {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+        storyMenu.SetActive(false);
+    }
+
+    public void SelectCreditsMenu() {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void SelectStoryMenu() {
+        mainMenu.SetActive(false);
+        storyMenu.SetActive(true);
+    }
+
+    public void LoadScene(string level) {
+        SceneManager.LoadScene(level);
+    }
 }
