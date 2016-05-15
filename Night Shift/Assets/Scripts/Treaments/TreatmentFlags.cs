59251260142be0a65e2a41ce6cce74fcb8670588
Pascal Dagenais <pascal.dagenais@hotmail.com>
2016-05-15 15:06:18 -0400

    Lots of minor fixes and placements of healing spots

--- Night Shift/Assets/Scripts/Treaments/TreatmentFlags.cs
+++ Night Shift/Assets/Scripts/Treaments/TreatmentFlags.cs
@@ -9,11 +9,11 @@ public static class TreatmentFlags
     public static bool isSurgeryOccupied { get; set; }
     public static bool isExorcismOccupied { get; set; }
 
-    public static readonly Vector3 VomitoriumPosition = new Vector3(-9.0f, 7.25f, -1.0f);
-    public static readonly Vector3 SurgeryPosition = new Vector3(-1.0f, 7.25f, -1.0f);
-    public static readonly Vector3 PsychologyPosition = new Vector3(12.0f, 3.5f, -1.0f);
-    public static readonly Vector3 HemorhagiePosition = new Vector3(12.0f, -5.0f, -1.0f);
-    public static readonly Vector3 ExorcismPosition = new Vector3(0.0f, -12.25f, -1.0f);
-    public static readonly Vector3 EntrancePosition = new Vector3(-22.0f, 1.0f, -1.0f);
+    public static readonly Vector3 VomitoriumPosition = new Vector3(-12.0f, 9.4f, -1.0f);
+    public static readonly Vector3 SurgeryPosition = new Vector3(3.750f, 12.15f, -1.0f);
+    public static readonly Vector3 PsychologyPosition = new Vector3(18.75f, 2f, -1.0f);
+    public static readonly Vector3 HemorhagiePosition = new Vector3(17.25f, -5.0f, -1.0f);
+    public static readonly Vector3 ExorcismPosition = new Vector3(3.35f, -13.6f, -1.0f);
+    public static readonly Vector3 EntrancePosition = new Vector3(-20.0f, 1.0f, -1.0f);
 }
  
