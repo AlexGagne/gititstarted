b7e2946ad28e38c22d07952ec0e48425009b530e
ExaltedBagel <jacobmaxime@hotmail.com>
2016-05-15 05:14:43 -0400

    New Prefabs for units including moral and hp bar Merge of Max and Alex carried further Initial Steve does not move -BUG

---
+++ Night Shift/Assets/StatBar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatBar : MonoBehaviour {
+
+    public float radius;
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = gameObject.transform.parent.GetChild(0).position + new Vector3(0, radius);
+    }
+}
